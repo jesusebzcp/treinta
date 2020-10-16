989d2e89c34389149e73ed95be6486b76236ff6f
jesus <jesusebzcp19@gmail.com>
2020-10-16 09:24:44 -0500

    delete api

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
-    APIKEY: "AIzaSyA1NGox8zbtat6j_NRDaypOjwut5KHNlBg",
-    AUTHDOMAIN: "demorestaurant-af381.firebaseapp.com",
-    DATABASEURL: "https://demorestaurant-af381.firebaseio.com",
-    PROJECTID: "demorestaurant-af381",
-    STORAGEBUCKET: "demorestaurant-af381.appspot.com",
-    MESSAGINGSENDERID: "257136313226",
-    APPID: "1:257136313226:web:6af1688b6c5ce23e4afe73",
-    MEASUEMENTID: "G-MV2XT28SGJ",
-    APIMAP: "AIzaSyArVhfk_wHVACPwunlCi1VP9EUgYZcnFpQ",
+    APIKEY: "**********",
+    AUTHDOMAIN: "**************",
+    DATABASEURL: "*****************",
+    PROJECTID: "**************",
+    STORAGEBUCKET: "******************",
+    MESSAGINGSENDERID: "*****************",
+    APPID: "*****************",
+    MEASUEMENTID: "**************",
+    APIMAP: "*****************",
   },
 };
